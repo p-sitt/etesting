2803404a63860aef2348ddafe70bbb0f2284ed67
p-sitt <p-sitt@hotmail.com>
2019-08-07 15:01:41 +0700

    update master template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -59,7 +59,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="/img/man.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
